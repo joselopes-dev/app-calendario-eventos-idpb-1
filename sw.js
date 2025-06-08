30b7e8c0c0c83e829fd0a7e76d22b0d4146ccd67
joselopes-dev <140862898+joselopes-dev@users.noreply.github.com>
2025-06-08 19:38:27 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,8 +4,8 @@ const ASSETS = [
   '/',
   '/index.html',
   '/manifest.json',
-  './icon-192.png',
-  './icon-512.png',
+  './icon-192x192.png',
+  './icon-512x512.png',
   'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css',
   // Adicione outros assets estáticos aqui (CSS, JS, fonts)
 ];
